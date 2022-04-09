a96988153ff6650fb61cc1672530afd79c5f0eed
rehan6099 <103304908+rehan6099@users.noreply.github.com>
2022-04-09 14:13:37 +0530

    update c2-generic-variables.tf

--- c2-generic-variables.tf
+++ c2-generic-variables.tf
@@ -7,5 +7,15 @@ variable "aws_region" {
   default = "us-east-1"  
 }
 
-
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "SAP"
+}
 
